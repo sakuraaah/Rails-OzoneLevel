2356b83f9f9c5d15c5a262b119264519cef2e253
sakuraaah <niktro30@gmail.com>
2024-04-26 14:12:08 +0300

    some build fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js"
+
+# custom
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.0/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
